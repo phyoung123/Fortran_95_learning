2097bf82ab39c8817ec7c0dad7ba06f122e82d90
phyang <fyxu0106@gmail.com>
2023-06-13 20:24:10 +0800

    add code and 4.2.txt

---
+++ chapter_4/code/EX04011.F90
@@ -0,0 +1,10 @@
+PROGRAM EX0411
+COMPLEX :: A,B
+A=(1.0,1.0)
+B=(1.0,2.0)
+WRITE(*,*) "A+B=",A+B
+WRITE(*,*) "A-B=",A-B
+WRITE(*,*) "A*B=",A*B
+WRITE(*,*) "A/B=",A/B
+STOP
+END
---
+++ chapter_4/code/EX0407.F90
@@ -0,0 +1,6 @@
+program ex0407
+REAL(KIND=4) :: A
+A = 2.5+3.0/2
+WRITE(*,*) "2.5+3.0/2=", A
+STOP
+END
